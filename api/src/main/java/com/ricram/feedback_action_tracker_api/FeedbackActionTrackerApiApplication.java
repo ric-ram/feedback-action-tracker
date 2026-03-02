c0761c0b4d26064789ace1a2479b7c8dd4d2bf69
Ricram <ricram.log@gmail.com>
2026-03-02 17:53:43 +0000

    Initialize Spring boot application

---
+++ api/src/main/java/com/ricram/feedback_action_tracker_api/FeedbackActionTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.ricram.feedback_action_tracker_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackActionTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackActionTrackerApiApplication.class, args);
+	}
+
+}
